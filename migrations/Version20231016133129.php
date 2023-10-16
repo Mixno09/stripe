75adab509b210bde1795f275f59622d76e1c7703
Mixno09 <mixno09@mail.ru>
2023-10-16 16:39:21 +0300

    migration

--- migrations/Version20231016133129.php
+++ migrations/Version20231016133129.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20231013145029 extends AbstractMigration
+final class Version20231016133129 extends AbstractMigration
 {
     public function getDescription(): string
     {
